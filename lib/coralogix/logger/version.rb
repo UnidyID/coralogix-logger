d8803a449037281e0cb46bd57a589d463d41a1e8
Fran Zekan <zekan.fran369@gmail.com>
2024-11-21 20:48:51 +0100

    init

---
+++ lib/coralogix/logger/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Coralogix
+  module Logger
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/coralogix/logger_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Coralogix::Logger do
+  it "has a version number" do
+    expect(Coralogix::Logger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
